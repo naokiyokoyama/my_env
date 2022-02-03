e224110ba203d15219f0208fae5bab1e20e64b64
naokiyokoyama <naoki.yokoyama7@gmail.com>
2022-02-03 17:10:06 -0500

    Update replace_all_in_file.py

--- my_useful/replace_all_in_file.py
+++ my_useful/replace_all_in_file.py
@@ -9,10 +9,13 @@ args = parser.parse_args()
 with open(args.file_path) as f:
     data = f.read()
 
-occurrences = data.count(args.key)
-data = data.replace(args.key, args.value)
+occurrences = data.count(args.str_to_replace)
+data = data.replace(args.str_to_replace, args.str_to_replace_with)
 
 with open(args.file_path, 'w') as f:
     f.write(data)
 
-print(f"Replaced {occurrences} occurrences of {args.key} with {args.value}.")
+print(
+    f"Replaced {occurrences} occurrences of {args.str_to_replace} "
+    f"with {args.str_to_replace_with}."
+)
