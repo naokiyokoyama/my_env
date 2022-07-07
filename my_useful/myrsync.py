973d0a7b357f30ba77636a6d2c62deaf453319e7
Naoki Yokoyama <naokiyokoyama@github.com>
2022-07-07 13:56:56 -0400

    made myrsync.py recursive

--- my_useful/myrsync.py
+++ my_useful/myrsync.py
@@ -3,7 +3,7 @@ import argparse
 import os
 
 copy_cmd = (
-    "rsync -av --progress -e "
+    "rsync -rav --progress -e "
     "'ssh -T -c chacha20-poly1305@openssh.com -o Compression=no -x' "
     "{exclude} {src} {dst}"
 )
