fc1aef1f17ab1369a6ce6449e76fce441a252a44
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-09-22 20:53:54 -0400

    using basename as defaut suffix

--- my_useful/speedup_video.py
+++ my_useful/speedup_video.py
@@ -1,9 +1,12 @@
 import argparse
+import os.path as osp
 
 from moviepy.editor import VideoFileClip, vfx
 
 
 def main(video, speedup, out_path):
+    if out_path is None:
+        out_path = osp.basename(video)
     clip = VideoFileClip(video)
     clip = clip.fx(vfx.speedx, speedup)
     out_path = f"{speedup}_{out_path}"
@@ -15,6 +18,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("video")
     parser.add_argument("speedup", type=float, help="Speed up factor")
-    parser.add_argument("-o", "--out-path", default="out.mp4")
+    parser.add_argument("-o", "--out-path")
     args = parser.parse_args()
     main(args.video, args.speedup, args.out_path)
