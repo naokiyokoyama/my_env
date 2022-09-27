75656418092bbf471a51691bcee0caf6345eba45
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-09-27 04:22:23 -0400

    added clip video script

---
+++ my_useful/clip_video.py
@@ -0,0 +1,23 @@
+import argparse
+import os.path as osp
+
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+
+def main(video, out_path, start, end):
+    if out_path is None:
+        out_path = osp.basename(f"{start}_{end}_{video}")
+    clip = VideoFileClip(video)
+    trimmed_clip = clip.subclip(start, end)
+    trimmed_clip.write_videofile(out_path, fps=30)
+    print(f"Saved output video to {out_path}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("video")
+    parser.add_argument("start")
+    parser.add_argument("end")
+    parser.add_argument("-o", "--out-path")
+    args = parser.parse_args()
+    main(args.video, args.out_path, args.start, args.end)
