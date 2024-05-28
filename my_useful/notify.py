6aa427979246d6cce76b3220026634eef8f67c15
naokiyokoyama <naoki.yokoyama7@gmail.com>
2024-05-28 13:25:10 -0700

    Switching to ntfy.sh for notifications

--- my_useful/notify.py
+++ my_useful/notify.py
@@ -1,14 +1,14 @@
+import os
 import subprocess
 import sys
+
+ntfy_topic = os.environ["NTFY_TOPIC"]
 
 words = sys.argv[1:]
 sentence = "+".join(words)
 sentence = sentence.replace(" ", "+").replace("[", "_").replace("]", "_")
 
-cmd = (
-    f"curl -s -o /dev/null "
-    f'"https://maker.ifttt.com/trigger/notify/with/key/$IFTTT_KEY?value1={sentence}"'
-)
+cmd = f'curl -d "{sentence}" ntfy.sh/{ntfy_topic}'
 
 subprocess.check_call(cmd, shell=True)
-print(f"Push notification <{sentence.replace('+', ' ')}> sent to app.")
+print(f"Push notification <{sentence.replace('+', ' ')}> sent to ntfy topic.")
